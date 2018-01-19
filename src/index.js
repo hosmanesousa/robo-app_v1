86567ae4efee2f5705ef3cbf309ae27257063d8f
Hosmane Sousa <hosmane.sousa@gmail.com>
2018-01-19 17:39:06 -0500

    robo-app files 1

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+//import Card from './Card.js';
+import App from './containers/App'; //
+import registerServiceWorker from './registerServiceWorker';
+import 'tachyons';
+//import { robots } from './robots.js'
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
